5b0e606a55a7f6c2c03e19315401997cdc7401be
Karthik Joshi <67888831+kdj309@users.noreply.github.com>
2024-05-19 07:58:09 +0000

    added pine cone vector store

--- ragchatbot/chatbot.py
+++ ragchatbot/chatbot.py
@@ -1,12 +1,26 @@
-import json
 import os
 import bs4
+from langchain_pinecone import PineconeVectorStore
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import JSONLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import ConversationalRetrievalChain
-from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain import hub
 from langchain_groq import ChatGroq
+from dotenv import load_dotenv,find_dotenv
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+
+load_dotenv(find_dotenv())
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 
 #loading the json from the json converting them to Langchain document
@@ -14,22 +28,84 @@ loader = JSONLoader(file_path="./products.json", jq_schema=".products[]", text_c
 documents = loader.load()
 
 #splitting the documents
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 all_splits = text_splitter.split_documents(documents)
 
 #creating the faiss index using hugging face model
-vectorstore = FAISS.from_documents(all_splits, HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2"))
+vectorstore = PineconeVectorStore.from_documents(all_splits, HuggingFaceEmbeddings(),index_name=os.environ.get("PINECONE_INDEX_NAME"))
 
+retriever = vectorstore.as_retriever(search_type="similarity")
 
 #initializing the GROQ chat
-llm = ChatGroq(temperature=0, model_name="llama3-8b-8192",groq_api_key=os.environ.get("GROQ_API_KEY"))
+llm = ChatGroq(temperature=0, model_name="llama3-70b-8192",groq_api_key=GROQ_API_KEY)
 
 #Langchain chain for QA
-chain = ConversationalRetrievalChain.from_llm(llm,
-                                              vectorstore.as_retriever(),
-                                              return_source_documents=True)
+prompt = hub.pull("rlm/rag-prompt")
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
 
-result = chain({"question": "list the products of Mitera brand", "chat_history": []})
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
 
-print(result['answer'])
+
+### Answer question ###
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+
+### Statefully manage chat history ###
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+    verbose=True
+)
+
+print(conversational_rag_chain.invoke(
+    {"input": "which one is the costliest product among the listed products hint: filter by price"},
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in `store`.
+)["answer"])
 
